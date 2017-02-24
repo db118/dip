c2621502ce44bc3ef42d4169e1d39d51e597b39a
Michael_Rodriguez <mmr94@txstate.edu>
2017-02-23 20:33:47 -0600

    Update project1.m

--- project1.m
+++ project1.m
@@ -2,11 +2,11 @@
 % 
 A = [0:255]';
 image2 = repmat(uint8(A),1,256);
-% imshow(image2)
+figure, imshow(image2)
 
 
 image3 = repmat(uint8(A'),256,1);
-% imshow(image3)
+figure, imshow(image3)
 
 image4 = zeros(256,256, 'uint8');
 
@@ -15,18 +15,20 @@ for i = 1:256
         image4(i,j) = ((i-1)+(j-1))/2 ;
     end
 end
-%imshow(image4)
+figure, imshow(image4)
 
 image5 = zeros(512,512,'uint8');
 image5(1:256,1:256) = image4(1:256,1:256); % upper left
 image5(257:512,1:256) = image4(256:-1:1,1:256); % lower left
 image5(1:256,257:512) = image4(1:256,256:-1:1); % upper right
 image5(257:512,257:512) = image4(256:-1:1,256:-1:1); %lower right
+figure,imshow(image5)
 
-% imshow(image5)
+
+
 
 image6 = imread('~/Sample Images/lena.jpg');
-% imshow(image6)
+figure, imshow(image6)
 
 x = size(image6) * 2;
 
@@ -36,7 +38,7 @@ image7(257:512,1:256) = image6(256:-1:1,1:256); % lower left
 image7(1:256,257:512) = image6(1:256,256:-1:1); % upper right
 image7(257:512,257:512) = image6(256:-1:1,256:-1:1); %lower right
 
-% imshow(image7)
+figure, imshow(image7)
 
 
 imwrite(image4,'homework1_4.jpg')
@@ -50,8 +52,9 @@ image11 = zeros(256,256,3,'uint8');
 image11(:,:,1) = image2(:,:);
 image11(:,:,2) = image3(:,:);
 image11(:,:,3) = image4(:,:);
-% imshow(image11)
-% imwrite(image11, 'homework1_11.ppm')
+figure, imshow(image11)
+
+imwrite(image11, 'homework1_11.ppm')
 
 
 image13 = imread('~/Sample Images/lena_color.jpg');
@@ -65,10 +68,12 @@ image14(1:256,1:256,:) = image13(1:256,1:256,:); % upper left
 image14(257:512,1:256,:) = image13(256:-1:1,1:256,:); % lower left
 image14(1:256,257:512,:) = image13(1:256,256:-1:1,:); % upper right
 image14(257:512,257:512,:) = image13(256:-1:1,256:-1:1,:); %lower right
-imshow(image14)
+figure, imshow(image14)
 
 imwrite(image14, 'homework1_14.pnm')
 
 
-
+display('Press any key to continue...');
+pause
+delete(findall(0,'Type','figure'))
 
