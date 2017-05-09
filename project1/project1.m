beba7bf67e9a851eb80522d0c139bd8c1a0b07d8
db118 <d_b118@txstate.edu>
2017-05-09 02:11:26 -0500

    finishing project 3

--- project1/project1.m
+++ project1/project1.m
@@ -1,90 +1,73 @@
-%% Question 1
-image1 = 150 * ones(256,256, 'uint8');
-figure, imshow(image1)
-
-%%Question 2
+% image1 = 150 * ones(256,256, 'uint8');
+% 
 A = [0:255]';
 image2 = repmat(uint8(A),1,256);
-figure, imshow(image2)
+% imshow(image2)
 
-%% Question 3
+
 image3 = repmat(uint8(A'),256,1);
-figure, imshow(image3)
+% imshow(image3)
 
-
-%% Question 4
 image4 = zeros(256,256, 'uint8');
+
 for i = 1:256
     for j = 1:256
         image4(i,j) = ((i-1)+(j-1))/2 ;
     end
 end
-figure, imshow(image4)
+%imshow(image4)
 
-
-%% Question 5
 image5 = zeros(512,512,'uint8');
 image5(1:256,1:256) = image4(1:256,1:256); % upper left
 image5(257:512,1:256) = image4(256:-1:1,1:256); % lower left
 image5(1:256,257:512) = image4(1:256,256:-1:1); % upper right
 image5(257:512,257:512) = image4(256:-1:1,256:-1:1); %lower right
-figure, imshow(image5)
 
+% imshow(image5)
 
-%% Question 6
 image6 = imread('~/Sample Images/lena.jpg');
-figure,imshow(image6)
+% imshow(image6)
 
+x = size(image6) * 2;
 
-%% Question 7
-[row,column] = size(image6) * 2;
-image7 = zeros(row,column,'uint8');
+image7 = zeros(x(1),x(2),'uint8');
 image7(1:256,1:256) = image6(1:256,1:256); % upper left
 image7(257:512,1:256) = image6(256:-1:1,1:256); % lower left
 image7(1:256,257:512) = image6(1:256,256:-1:1); % upper right
 image7(257:512,257:512) = image6(256:-1:1,256:-1:1); %lower right
-figure, imshow(image7)
 
-%% Question 8
+% imshow(image7)
+
+
 imwrite(image4,'homework1_4.jpg')
 
-%% Question 9
 imwrite(image5,'homework1_5.gif')
 
-%% Question 10
 imwrite(image7, 'homework1_7.pgm')
 
-
-%% Question 11
 image11 = zeros(256,256,3,'uint8');
+
 image11(:,:,1) = image2(:,:);
 image11(:,:,2) = image3(:,:);
 image11(:,:,3) = image4(:,:);
-figure, imshow(image11)
+% imshow(image11)
+% imwrite(image11, 'homework1_11.ppm')
 
-%% Question 12
-imwrite(image11, 'homework1_11.ppm')
 
-%% Question 13
 image13 = imread('~/Sample Images/lena_color.jpg');
 
-%% Question 14
+
 y = size(image13)*2
+
 image14 = zeros(y(1),y(2),y(3)/2,'uint8');
+
 image14(1:256,1:256,:) = image13(1:256,1:256,:); % upper left
 image14(257:512,1:256,:) = image13(256:-1:1,1:256,:); % lower left
 image14(1:256,257:512,:) = image13(1:256,256:-1:1,:); % upper right
 image14(257:512,257:512,:) = image13(256:-1:1,256:-1:1,:); %lower right
-figure, imshow(image14)
+imshow(image14)
 
-%% Question 15
 imwrite(image14, 'homework1_14.pnm')
-
-
-disp('Enter any key to close all figures: ')
-pause
-close all 
-
 
 
 
---
+++ project3/project3.m
@@ -0,0 +1,251 @@
+function project2(varargin);
+global current_image
+global preview_image
+global open_button
+global save_button
+global preview_button
+global apply_button
+global filter_popup
+global bright_slider
+global bright_title
+global bright_value
+global contrast_slider
+global contrast_title
+global contrast_value
+global orig_bright
+global orig_contrast
+global FIG
+global FIG2
+global FIG3
+function_name='project2';
+if nargin<1,
+    action='initialize';
+else
+    action=varargin{1};
+end;
+
+if strcmp(action,'initialize'),
+    figNumber=figure( ...
+        'Name','Project 2', ...
+        'NumberTitle','off', ...
+        'Position',[100 100 800 600], ...
+        'Visible','off');
+     colordef(figNumber,'white')
+     current_image = axes('Position',[0.05 0.55 0.60 0.426667]);
+     title('Current');
+     preview_image =axes('Position',[0.05 0.05 0.60 0.426667]);
+     title('Previous');  
+%====================================
+    % Information for all buttons
+    top=0.95;
+    left=0.81;
+    btnWid=0.15;
+    btnHt=0.06;
+    % Spacing between the button and the next command's label
+    spacing=0.03*0.5;
+%====================================
+    % The OPEN IMAGE button
+    btnNumber=1;
+    yPos=top-(btnNumber-1)*(btnHt+spacing);
+    labelStr='Open Image';
+    callbackStr=strcat(function_name,'(''open'')');  
+    % Generic button information
+    btnPos=[left yPos-btnHt btnWid btnHt];
+    open_button = uicontrol( ...
+        'Style','pushbutton', ...
+        'Units','normalized', ...
+        'Position',btnPos, ...
+        'String',labelStr, ...
+        'Callback',callbackStr);
+%====================================
+    % The SAVE_IMAGE button
+    btnNumber=2;
+    yPos=top-(btnNumber-1)*(btnHt+spacing);
+    labelStr='Save Image';
+    callbackStr=strcat(function_name,'(''save'')');
+     % Generic button information
+    btnPos=[left yPos-btnHt btnWid btnHt];
+    save_button = uicontrol( ...
+        'Style','pushbutton', ...
+        'Units','normalized', ...
+        'Position',btnPos, ...
+        'String',labelStr, ...
+        'Callback',callbackStr);
+%====================================
+    % The Apply Changes Button
+    btnNumber = 3;
+    yPos= top-(btnNumber-1)*(btnHt+spacing);
+    labelStr='Apply Changes';
+    callbackStr=strcat(function_name,'(''apply'')');
+    btnPos = [left yPos-btnHt btnWid btnHt];
+    apply_button = uicontrol(...
+        'Style', 'pushbutton', ...
+        'Units', 'normalized',...
+        'Position', btnPos, ...
+        'String', labelStr,...
+        'Callback', callbackStr);
+    
+%====================================
+    % The Preview Button
+    btnNumber = 4;
+    yPos= top-(btnNumber-1)*(btnHt+spacing);
+    labelStr='Preview';
+    callbackStr=strcat(function_name,'(''preview'')');
+    btnPos = [left yPos-btnHt btnWid btnHt];
+    preview_button = uicontrol(...
+        'Style', 'pushbutton', ...
+        'Units', 'normalized',...
+        'Position', btnPos, ...
+        'String', labelStr,...
+        'Callback', callbackStr);
+
+%====================================
+    % The Filter Popup
+    btnNumber= 5;
+    yPos=top-(btnNumber-1)*(btnHt+spacing);
+    labelStr= {'3x3 Low Pass','5x5 Low Pass','7x7 Low Pass','9x9 Low Pass'...
+               'HighPass','HighBoost','Histogram equalization'...
+               'Freq Low Pass', 'Freq High Pass', 'Freq HighBoost'...
+               'Freq Band-Pass', 'Freq Band-Stop'};
+    callbackStr=strcat(function_name,'(''filter'')');
+     % Generic Popup information
+    btnPos=[left yPos-btnHt btnWid btnHt];
+    filter_popup = uicontrol( ...
+        'Style','popupmenu', ...
+        'Units','normalized', ...
+        'Position',btnPos, ...
+        'String',labelStr, ...
+        'Callback',callbackStr);
+
+%====================================
+    % The Bright slider
+    btnNumber=6;
+    yPos=top-(btnNumber-1)*(btnHt+spacing);
+    callbackStr = strcat(function_name,'(''bright'')');
+     % Generic button information
+    btnPos=[left (yPos-btnHt) btnWid btnHt];
+    bright_slider = uicontrol( ...
+        'Style','slider', ...
+        'Units','normalized', ...
+        'Position',btnPos, ...
+        'Min', 0 ,...
+        'Max', 255 ,...
+        'Value', 130 ,...
+        'Callback',callbackStr);
+    bright_value = floor(get(bright_slider,'Value'));
+    % Add a text uicontrol to label the slider.
+    bright_title = uicontrol(...
+        'Style','text',...
+        'Units','normalized', ...
+        'Position',[left (yPos-btnHt+.08) btnWid btnHt-0.04],...
+        'String','Brightness');
+
+%====================================
+    % The Contrast slider
+    btnNumber=7;
+    yPos=top-(btnNumber-1)*(btnHt+spacing);
+    callbackStr = strcat(function_name,'(''contrast'')');
+     % Generic button information
+    btnPos=[left yPos-btnHt btnWid btnHt];
+    contrast_slider = uicontrol( ...
+        'Style','slider', ...
+        'Units','normalized', ...
+        'Position',btnPos, ...
+        'Min', 0 ,...
+        'Max', 255 ,...
+        'Value', 130,...
+        'Callback',callbackStr);
+    contrast_value = floor(get(contrast_slider,'Value'));
+    % Add a text uicontrol to label the slider.
+    contrast_title = uicontrol(...
+        'Style','text',...
+        'Units','normalized', ...
+        'Position',[left (yPos-btnHt+.08) btnWid btnHt-0.04],...
+        'String','Contrast');
+    
+    % Now uncover the figure
+    set(figNumber,'Visible','on');
+    
+elseif strcmp(action,'save'),
+    save_image(FIG);
+elseif strcmp(action,'open'),
+    axes(current_image);
+    FIG=open_image();
+    fig_size=size(FIG);
+    if (max(size(fig_size >= 4)))
+        FIG=FIG(:,:,:,1);
+    end;
+    fig_size=size(FIG);
+    if (max(size(fig_size) >= 3))
+        FIG=rgb2ycbcr(FIG);
+    end
+    fig_size=size(FIG);
+    if (max(size(fig_size) == 2))
+        temp=FIG(:,:,1);
+        FIG=temp;
+    end
+    imshow(ycbcr2rgb(FIG));
+    title('Current Image')
+    FIG2 = FIG;
+    axes(preview_image)
+    imshow(ycbcr2rgb(FIG2));
+    title('Preview Image')
+elseif strcmp(action,'preview'),
+    axes(preview_image);
+    imshow(ycbcr2rgb(FIG2));
+    title('Preview Image');
+elseif strcmp(action,'apply'),
+    axes(current_image);
+    FIG = FIG2;
+    imshow(ycbcr2rgb(FIG));
+    title('Current Image');
+elseif strcmp(action,'bright')
+    bright_value = floor(get(bright_slider,'Value'));
+    bright = bright_value/255;
+    if (bright >= 0.5)
+        FIG2(:,:,1) = FIG2(:,:,1) + 1;
+    end
+    if (bright < 0.5)
+        FIG2(:,:,1) = FIG2(:,:,1) - 1;
+    end
+elseif strcmp(action,'contrast')
+    contrast_value = floor(get(contrast_slider,'Value'));
+    contrast = contrast_value/255;
+    if (contrast >= 0.5)
+        FIG2(:,:,1) = imadjust(FIG2(:,:,1),[0 2*contrast-1],[0 1]); 
+    end
+    if (contrast < 0.5)
+        FIG2(:,:,1) = imadjust(FIG2(:,:,1),[2*contrast 1],[0 1]); 
+    end
+elseif strcmp(action,'filter')
+    value = get(filter_popup,'Value');
+    string_list = get(filter_popup,'String');
+    popup_string = string_list{value};
+    if strcmp(popup_string,  '3x3 Low Pass')
+        FIG2(:,:,1) = imfilter(FIG(:,:,1), ones(3,3)/9);
+    elseif strcmp(popup_string,'5x5 Low Pass')
+        FIG2(:,:,1) = imfilter(FIG(:,:,1) ,ones(5,5)/25);
+    elseif strcmp(popup_string, '7x7 Low Pass')
+        FIG2(:,:,1) = imfilter(FIG(:,:,1),ones(7,7)/49);
+    elseif strcmp(popup_string, '9x9 Low Pass')
+        FIG2(:,:,1) = imfilter(FIG(:,:,1),ones(9,9)/81);        
+    elseif strcmp(popup_string, 'HighPass')
+        filterhp = [-1,-1,-1;-1,8,-1;-1,-1,-1];
+        FIG2(:,:,1) = imfilter(FIG(:,:,1),filterhp);
+    elseif strcmp(popup_string,'HighBoost')
+        FIG3(:,:,1)=imfilter(imsubtract(FIG(:,:,1),25),[-1 -1 -1;-1 8 -1;-1 -1 -1]);
+        FIG2(:,:,1) = imadd(FIG(:,:,1), FIG3(:,:,1))
+    elseif strcmp(popup_string, 'Histogram equalization')
+        FIG2(:,:,1) = histeq(FIG(:,:,1))
+    elseif strcmp(popup_string,  'Freq Low Pass')
+        
+    elseif strcmp(popup_string,  'Freq High Pass')
+        
+    elseif strcmp(popup_string,  'Freq HighBoost')
+        
+    elseif strcmp(popup_string,  'Freq Band-Pass')
+        
+    elseif strcmp(popup_string,  'Freq Band-Stop')
+    end
+    
+end;
